9a8d9bf77de537120457b0f579c98d92b210d891
fazilwork <fazilwork225@gmail.comgit config --global user.email>
2024-08-12 17:12:13 +0500

    first commit

---
+++ git2.py
@@ -0,0 +1,54 @@
+
+#                     первый:
+# a = int(input("число"))
+# b = int(input("число"))
+# try:b = a/b
+# except ZeroDivisionError:
+#     print("говно")
+        
+#                     второй:
+# try:
+#     file_tet = str(input("пиши цифру"))
+#     with open("dsflj.txt", "r") as file:
+#         content = file.read(file_tet)
+# except FileNotFoundError:
+#     print("sldf;sk")        
+#                      ТРЕТИЙ:
+# try:
+#     recuest = int(input("пиши число"))
+#     b = 100
+#     f = recuest / 0
+# except ZeroDivisionError as f:
+#     print("пиши бля число")
+# except ValueError:
+#     print("dhddh")        
+#                        ЧЕТВЕРТЫЙ: 
+# try:
+#     d = "sdgjsjgk"
+#     with open("3423532ple.txt", "w") as file:
+#         c = file.write(d)
+# except FileNotFoundError:
+#     print("o;osduigo;dir")        
+
+#                         ПЯТЫЙ:
+
+
+try:
+    a = int(input("пиши "))
+except ValueError:
+
+
+    print("хрень")
+    
+else:
+        print(a)
+finally:
+    print("дааа")
+    
+
+
+
+
+
+
+
